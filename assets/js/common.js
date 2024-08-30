835173205f373061b0207594bd41f01397be7da6
1uyeon <45332354+1uyeon@users.noreply.github.com>
2024-08-30 16:36:45 +0900

    메인/프로필 작업중

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,14 @@
 
 document.addEventListener('DOMContentLoaded', () => { 
-    // 로딩 화면
-    setTimeout(function () {
-        $("html").css("overflow",'auto'); 
-        $(".loading").addClass("disable")
-     }, 1300);
+  AOS.init();
 
-    // px -> rem 변환
+  // 로딩 화면
+  setTimeout(function () {
+    $("html").css("overflow",'auto'); 
+    $(".loading").addClass("disable")
+  }, 1300);
+
+  // px -> rem 변환
     const fontResponsive = () => {
         let bodyFontSize = 16;
         let maxWidth = 1920;
@@ -23,49 +25,4 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     fontResponsive(); 
     window.addEventListener('resize', fontResponsive);
-
-    // 🍅 움직이기
-    const draggable = ($target) => {
-        let isPress = false,
-            prevPosX = 0,
-            prevPosY = 0;
-        
-        // 마우스 눌림 시작
-        window.onmousedown = start;
-        // 마우스 버튼을 떼면 드래그 종료
-        window.onmouseup = end;
-        // 마우스 이동 중
-        window.onmousemove = move;
-          
-       
-        function start(e) {
-          prevPosX = e.clientX;
-          prevPosY = e.clientY;
-      
-          isPress = true;
-        }
-      
-        function move(e) {
-          if (!isPress) return;
-      
-          const posX = prevPosX - e.clientX; 
-          const posY = prevPosY - e.clientY; 
-          
-          prevPosX = e.clientX; 
-          prevPosY = e.clientY;
-          
-          $target.style.left = ($target.offsetLeft - posX) + "px";
-          $target.style.top = ($target.offsetTop - posY) + "px";
-        }
-      
-        function end() {
-          isPress = false;
-        }
-      }
-      
-      window.onload = () => {
-        const $target = document.querySelector(".tomato");
-        
-        draggable($target);
-      }
 });
